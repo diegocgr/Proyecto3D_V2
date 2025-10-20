af00fc9362b24a1a14377b8b1725d1f53b5acb8b
diegocgr <diegocgr@gmail.com>
2025-10-20 17:45:40 +0200

    Arreglos SceneDiego

--- Assets/Scripts/Navegar.cs
+++ Assets/Scripts/Navegar.cs
@@ -22,7 +22,7 @@ public class Navegar : MonoBehaviour
 
     public void NavegarRyeko()
     {
-        SceneManager.LoadScene("Scene3Ryeko");
+        SceneManager.LoadScene("Scene3RyekoFPS");
     }
 
     public void NavegarSergi()
